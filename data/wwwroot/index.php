593cbbf270de5257d3aa573ffd980eaa7f757376
Johannes von Rotz <jr@vrtz.ch>
2026-02-04 16:35:37 +0100

    show instructions if no POST data or ID available

--- data/wwwroot/index.php
+++ data/wwwroot/index.php
@@ -4,8 +4,9 @@ $path = "/var/www/html/pastedata";
 if($_SERVER["REQUEST_METHOD"] != "POST") {
 	/* we want to read an existing date */
 	if(!isset($_GET["id"])) {
-		header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
-		echo "Need id to know what to read";
+		header("Content-Type: text/html");
+		header($_SERVER["SERVER_PROTOCOL"]." 200 OK");
+		echo "<html><head><title>Paste</title></head><body><h1>Paste</h2><p>This is a web based clipboard. To paste stuff, do something like this:</p><pre>echo helloworld | curl -X POST --data @- https://".$_SERVER["SERVER_NAME"]."/</pre><p>You will get an identification string. To retrieve stuff do:</p><pre>curl https://".$_SERVER["SERVER_NAME"]."/?id=&lt;identification-string&gt;</pre><hr><small>Copyright (c) 2026 J. von Rotz <a href=\"https://github.com/jovoro/paste\"><address>https://github.com/jovoro/paste</address></a></small></body></html>";
 		exit();
 	}
 
